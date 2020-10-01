e0e4cf7742d8d9ba76ac2c6cc1a86cbaa0aa99bd
Thomas Garayua <tgarayua@gmail.com>
2020-10-01 03:18:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -153,6 +153,8 @@ end
 def team_colors(team_name)
   game_hash.each do |location, team_data|
     binding.pry
-  
+  if team_data[:team_name] == team_name
+    return team_data[:colors]
+  end
   end
 end
