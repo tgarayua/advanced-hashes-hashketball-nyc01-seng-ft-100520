f4617227579d852ecff3b909514488d8d935165f
Thomas Garayua <tgarayua@gmail.com>
2020-10-01 02:23:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -139,7 +139,7 @@ def num_points_scored(player_name)
 end
 
 
-def num_points_scored(player_name)
+def shoe_size(player_name)
   game_hash.each do |location, team_data|
     team_data[:players].each do |player|
       if player[:player_name] == player_name
