ca8b9a2a198d1fb2f103c69acccfd4772b5fa6eb
Thomas Garayua <tgarayua@gmail.com>
2020-10-01 00:56:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,8 +132,8 @@ def num_points_scored(name)
   score = nil
   
   game_hash.each do |home_or_away, team|
-    # binding.pry
-    score = home_or_away.each[2][3]
+    binding.pry
+    score = home_or_away[2][3]
   end
   score
 end
